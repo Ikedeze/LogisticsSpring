a17aae0caffed38284903cc1f3fa019c3cd5861d
Ikedeze <ikechuudeze14@gmail.com>
2026-07-10 18:35:55 +0200

    Fix hardcoded localhost connection in checkConnection runner

--- src/main/java/com/example/logistics/LogisticsApplication.java
+++ src/main/java/com/example/logistics/LogisticsApplication.java
@@ -2,23 +2,22 @@ package com.example.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
 public class LogisticsApplication {
 
-    @Bean
-    public org.springframework.boot.CommandLineRunner checkConnection(org.springframework.data.mongodb.core.MongoTemplate mongoTemplate) {
-        return args -> {
-            System.out.println("--------------------------------------------------");
-            System.out.println("CONNECTED TO DATABASE NAME: " + mongoTemplate.getDb().getName());
-            System.out.println("TOTAL SHIPMENTS FOUND: " + mongoTemplate.getCollection("shipment").countDocuments());
-            System.out.println("Tap this link: http://localhost:8080/swagger-ui.html");
-            System.out.println("--------------------------------------------------");
-        };
-    }
+//    @Bean
+//    public org.springframework.boot.CommandLineRunner checkConnection(org.springframework.data.mongodb.core.MongoTemplate mongoTemplate) {
+//        return args -> {
+//            System.out.println("--------------------------------------------------");
+//            System.out.println("CONNECTED TO DATABASE NAME: " + mongoTemplate.getDb().getName());
+//            System.out.println("TOTAL SHIPMENTS FOUND: " + mongoTemplate.getCollection("shipment").countDocuments());
+//            System.out.println("Tap this link: http://localhost:8080/swagger-ui.html");
+//            System.out.println("--------------------------------------------------");
+//        };
+//    }
     // .username("logistics_admin")
     //                .password("SecurePass123")
     public static void main(String[] args) {
